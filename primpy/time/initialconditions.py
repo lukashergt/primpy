2b7a0b246b9d56b240d5d1c9b20afff643652fea
lukas hergt <lh561@cam.ac.uk>
2020-03-26 00:42:38 +0000

    add ISIC_mtN to initial conditions

--- primpy/time/initialconditions.py
+++ primpy/time/initialconditions.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 """:mod:`primpy.time.initialconditions`: initial conditions for inflation."""
 import numpy as np
+from scipy.optimize import root_scalar
 from primpy.time.inflation import InflationEquationsT
+from primpy.solver import solve
+from primpy.events import InflationEvent, CollapseEvent
 
 
-class ISIC_NiPi(object):
+class InflationStartIC_NiPi(object):
     """Inflation start initial conditions given N_i, phi_i.
 
     Class for setting up initial conditions at the start of inflation, when
@@ -32,3 +35,50 @@ class ISIC_NiPi(object):
         y0[self.equations.idx['N']] = N_i
         y0[self.equations.idx['phi']] = phi_i
         y0[self.equations.idx['dphidt']] = dphidt_i
+
+
+class ISIC_mtN(InflationStartIC_NiPi):
+    """Inflation start initial conditions given potential mass/Lambda, N_tot, and N_i."""
+    def __init__(self, t_i, mass, N_tot, N_i, phi_i_bracket, K, Potential, t_end=1e300):
+        super(ISIC_mtN, self).__init__(t_i=t_i,
+                                       N_i=N_i,
+                                       phi_i=phi_i_bracket[-1],
+                                       K=K,
+                                       potential=Potential(mass),
+                                       t_end=t_end)
+        self.N_tot = N_tot
+        self.phi_i_bracket = phi_i_bracket
+
+    def __call__(self, y0, **ivp_kwargs):
+        events = [InflationEvent(self.equations, direction=+1, terminal=False),
+                  InflationEvent(self.equations, direction=-1, terminal=True),
+                  CollapseEvent(self.equations)]
+
+        def phii2Ntot(phi_i, kwargs):
+            """Helper function for scipy's optimize."""
+            ic = InflationStartIC_NiPi(t_i=self.x_ini,
+                                       N_i=self.N_i,
+                                       phi_i=phi_i,
+                                       K=self.equations.K,
+                                       potential=self.potential,
+                                       t_end=self.x_end)
+            sol = solve(ic, events=events, **kwargs)
+            if np.size(sol.t_events['Collapse']) > 0:
+                return 0 - self.N_tot
+            elif sol.success:
+                sol.total_efolds()
+                return sol.N_tot - self.N_tot
+            else:
+                print("sol = %s" % sol)
+                raise Exception("solve_ivp failed with message: %s" % sol.message)
+
+        output = root_scalar(phii2Ntot, args=(ivp_kwargs,), bracket=self.phi_i_bracket)
+        phi_i_new = output.root
+        super(ISIC_mtN, self).__init__(t_i=self.x_ini,
+                                       N_i=self.N_i,
+                                       phi_i=phi_i_new,
+                                       K=self.equations.K,
+                                       potential=self.potential,
+                                       t_end=self.x_end)
+        super(ISIC_mtN, self).__call__(y0=y0)
+        return phi_i_new, output
