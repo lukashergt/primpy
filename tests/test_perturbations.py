a5cb214f1127aa4a13434cbccf68075de651b3db
lukashergt <lh561@mrao.cam.ac.uk>
2020-07-14 13:08:34 +0100

    fix Omega_K0 to abs_Omega_K0 in tests

--- tests/test_perturbations.py
+++ tests/test_perturbations.py
@@ -124,13 +124,13 @@ def test_perturbations_discrete_time_efolds(K, f_i, abs_Omega_K0):
 
 @pytest.mark.parametrize('K', [-1, +1])
 @pytest.mark.parametrize('f_i', [10])  # FIXME: make 100, 1000 work as well
-@pytest.mark.parametrize('Omega_K0', [0.09, 0.009])
-def test_perturbations_continuous_time_vs_efolds(K, f_i, Omega_K0):
-    if -K * f_i * Omega_K0 >= 1:
+@pytest.mark.parametrize('abs_Omega_K0', [0.09, 0.009])
+def test_perturbations_continuous_time_vs_efolds(K, f_i, abs_Omega_K0):
+    if -K * f_i * abs_Omega_K0 >= 1:
         with pytest.raises(Exception):
-            setup_background(K=K, f_i=f_i, Omega_K0=Omega_K0)
+            setup_background(K=K, f_i=f_i, abs_Omega_K0=abs_Omega_K0)
     else:
-        bist, bisn = setup_background(K=K, f_i=f_i, Omega_K0=Omega_K0)
+        bist, bisn = setup_background(K=K, f_i=f_i, abs_Omega_K0=abs_Omega_K0)
         rtol = 5e-3
         atol = 1e-5
         ks_iMpc = np.logspace(-4, -1, 3 * 10 + 1)  # FIXME: make this work for smaller k?
@@ -145,13 +145,13 @@ def test_perturbations_continuous_time_vs_efolds(K, f_i, Omega_K0):
 
 @pytest.mark.parametrize('K', [-1, +1])
 @pytest.mark.parametrize('f_i', [10])  # FIXME: make 100, 1000 work as well
-@pytest.mark.parametrize('Omega_K0', [0.09, 0.009])
-def test_perturbations_large_scales_pyoscode_vs_background(K, f_i, Omega_K0):
-    if -K * f_i * Omega_K0 >= 1:
+@pytest.mark.parametrize('abs_Omega_K0', [0.09, 0.009])
+def test_perturbations_large_scales_pyoscode_vs_background(K, f_i, abs_Omega_K0):
+    if -K * f_i * abs_Omega_K0 >= 1:
         with pytest.raises(Exception):
-            setup_background(K=K, f_i=f_i, Omega_K0=Omega_K0)
+            setup_background(K=K, f_i=f_i, abs_Omega_K0=abs_Omega_K0)
     else:
-        bist, bisn = setup_background(K=K, f_i=f_i, Omega_K0=Omega_K0)
+        bist, bisn = setup_background(K=K, f_i=f_i, abs_Omega_K0=abs_Omega_K0)
         rtol = 0.05
         atol = 1e-5
         ks_iMpc = np.logspace(-2, 1, 50)  # FIXME: more samples without breaking github actions?
