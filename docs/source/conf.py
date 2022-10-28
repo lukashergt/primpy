20850386245abae081b47e820812bfd551626db1
Lukas Hergt <lthergt@posteo.de>
2022-10-27 22:01:28 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ pygments_style = 'sphinx'
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static']
+html_static_path = []
 # -- Extension configuration -------------------------------------------------
 
 autodoc_member_order = 'bysource'
