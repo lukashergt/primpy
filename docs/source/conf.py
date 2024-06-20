3afc214aab7357e2a23668e057b1deab675b7ede
lukashergt <lthergt@phas.ubc.ca>
2024-06-20 14:43:57 -0700

    include `intersphinx_mapping` for python built-ins such as `abc.ABC`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,6 +74,7 @@ numpydoc_class_members_toctree = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
     'numpy':('https://numpy.org/doc/stable/', None),
     'scipy':('https://docs.scipy.org/doc/scipy/', None),
     'matplotlib':('https://matplotlib.org/stable/', None),
