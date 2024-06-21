692f843b5644cfae5f6a68bf77f72b8b5a929efa
lukashergt <lthergt@phas.ubc.ca>
2024-06-20 17:16:09 -0700

    switch to numpydoc because napoleon is acting up; also use autosummary and copybutton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,8 @@ release = version
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.autosectionlabel',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
@@ -36,12 +38,16 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon',
-    # 'numpydoc',
+    'sphinx.ext.imgconverter',
+    'sphinx_copybutton',
+    'sphinx_autodoc_typehints',
+    #'sphinx.ext.napoleon',
+    'numpydoc',
     'matplotlib.sphinxext.plot_directive',
 ]
 
-templates_path = ['_templates']
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['../templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -53,6 +59,40 @@ exclude_patterns = []
 pygments_style = 'sphinx'
 
 
+# -- Options for autodoc -------------------------------------------------
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__call__',
+    'undoc-members': False,
+}
+autosummary_generate = True
+
+
+# -- Options for autosectionlabel------------------------------------------
+autosectionlabel_prefix_document = True
+
+
+# -- Options for sphinx-copybutton ----------------------------------------
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+
+# -- Options for numpydoc -------------------------------------------------
+numpydoc_class_members_toctree = False
+numpydoc_show_inherited_class_members = False
+#numpydoc_show_class_members = False 
+
+
+# -- Options for matplotlib extension ----------------------------------------
+plot_rcparams = {'savefig.bbox': 'tight'}
+plot_apply_rcparams = True  # if context option is used
+plot_include_source = True
+plot_html_show_source_link = False
+plot_html_show_formats = False
+plot_pre_code = "import numpy as np; from matplotlib import pyplot as plt"
+
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -62,13 +102,7 @@ pygments_style = 'sphinx'
 html_theme = 'sphinx_rtd_theme'
 
 html_static_path = []
-# -- Extension configuration -------------------------------------------------
 
-autodoc_member_order = 'bysource'
-
-plot_include_source = True
-
-numpydoc_class_members_toctree = False
 
 # -- Options for intersphinx extension ---------------------------------------
 
