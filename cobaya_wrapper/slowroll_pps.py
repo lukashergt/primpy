6b50ac02efe74efedadb00cec0aea816115cccf2
lukashergt <lthergt@phas.ubc.ca>
2025-05-06 23:49:04 +0200

    add `w_reh` as additional optional sampling parameter together with `rho_reh_GeV`

--- cobaya_wrapper/slowroll_pps.py
+++ cobaya_wrapper/slowroll_pps.py
@@ -29,6 +29,7 @@ class SlowRollPPS(ExternalPrimordialPowerSpectrum):
         A_s = params_values_dict.get('A_s')
         n_s = params_values_dict.get('n_s')
         rho_reh_GeV = params_values_dict.get('rho_reh_GeV')
+        w_reh = params_values_dict.get('w_reh')
         pot_kwargs = {}
         if 'phi0' in params_values_dict.keys():
             phi0 = params_values_dict.get('phi0')
@@ -59,11 +60,15 @@ class SlowRollPPS(ExternalPrimordialPowerSpectrum):
                       atol=1e-18, rtol=2.22045e-14, method='DOP853')
             if not b.success:
                 raise StepSizeError(b.message)
-            N_star = N_star if n_s is None else min(N_star, b.N_tot-0.1)
-            b.calibrate_scale_factor(N_star=N_star, rho_reh_GeV=rho_reh_GeV)
-            if n_s is not None:
-                b.set_ns(n_s=n_s, rho_reh_GeV=rho_reh_GeV,
-                         N_star_min=20, N_star_max=min(75, b.N_tot-0.1))
+            if N_star is not None and n_s is None:
+                N_star = N_star
+                b.calibrate_scale_factor(N_star=N_star, rho_reh_GeV=rho_reh_GeV)
+            elif w_reh is not None and rho_reh_GeV is not None:
+                b.calibrate_scale_factor(rho_reh_GeV=rho_reh_GeV, w_reh=w_reh)
+            else:
+                N_star = min(N_star, b.N_tot-0.1)
+                b.calibrate_scale_factor(N_star=N_star, rho_reh_GeV=rho_reh_GeV)
+                b.set_ns(n_s=n_s, rho_reh_GeV=rho_reh_GeV, N_star_min=20, N_star_max=N_star)
             # check whether the target A_s is met
             if abs(b.A_s - A_s) < atol + rtol * A_s:
                 break  # when the target is met, exit the loop
